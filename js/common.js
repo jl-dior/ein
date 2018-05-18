27da6bbf62b59b29575ac27d304f753a168e9e85
mac <mac@macdeMacBook-Pro.local>
2018-05-18 09:33:33 +0800

    init project

---
+++ js/common.js
@@ -0,0 +1,102 @@
+﻿$(function () {
+    $("#feedback_f .imgCode").click(function () {
+        var url = $(this).attr("src");
+        url += "1";
+        $(this).attr("src", url);
+    })
+
+    $("#feedback_f .btnSaveFeedback").click(function () {
+        var model = {};
+        var code = $("#feedback_f .txtCode").val();
+        if (code.length != 4) {
+            alert("请正确填写验证码");
+            $("#feedback_f .imgCode").click();
+            $("#feedback_f .txtCode").focus();
+            return false;
+        }
+        model.Title = $("#feedback_f .txtName").val();
+        model.Phone = $("#feedback_f .txtPhone").val();
+        model.Email = $("#feedback_f .txtEmail").val();
+        model.Content = $("#feedback_f .txtContent").val();
+        
+        if (model.Title.length < 2) {
+            alert("请填写姓名");
+            $("#feedback_f .txtName").focus();
+            return false;
+        }
+        if (!isPhone(model.Phone)) {
+            alert("请正确填写手机号码");
+            $("#feedback_f .txtPhone").focus();
+            return false;
+        }
+        if (!isEmail(model.Email)) {
+            alert("请正确填写邮箱");
+            $("#feedback_f .txtEmail").focus();
+            return false;
+        }
+        if (model.Content.length < 1) {
+            alert("请输入留言内容");
+            $("#feedback_f .txtContent").focus();
+            return false;
+        }
+        model.Ver = "cn";
+        model.ClassId = 203;
+        Feedback.Add(model, code, function (res) {
+            $("#feedback_f .imgCode").click();
+            if (res && res.value) {
+                if (res.value == -1) {
+                    alert("请正确填写验证码");
+                    $("#feedback_f .txtCode").focus();
+                    return false;
+                }
+                if (res.value == -2) {
+                    alert("请按正确格式完整填写信息");
+                    return false;
+                }
+                if (res.value > 0) {
+                    alert("留言成功");
+                    $("#feedback_f .txtName").val("");
+                    $("#feedback_f .txtPhone").val("");
+                    $("#feedback_f .txtEmail").val("");
+                    $("#feedback_f .txtContent").val("");
+                    $("#feedback_f .txtCode").val("");
+                    $("#feedback_f .closebook").click();
+                    return false;
+                }
+            }
+        })
+    });
+
+})
+
+
+
+
+/**
+* 检查字符串是否为合法手机号码
+* @param {String} 字符串
+* @return {bool} 是否为合法手机号码
+*/
+function isPhone(aPhone) {
+    var bValidate = RegExp(/^(0|86|17951)?(13[0-9]|15[012356789]|18[0-9]|14[57])[0-9]{8}$/).test(aPhone);
+    if (bValidate) {
+        return true;
+    }
+    else
+        return false;
+}
+/**
+* 检查字符串是否为合法email地址
+* @param {String} 字符串
+* @return {bool} 是否为合法email地址
+*/
+function isEmail(aEmail) {
+    var bValidate = RegExp(/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/).test(aEmail);
+    if (bValidate) {
+        return true;
+    }
+    else
+        return false;
+}
+
+
---
+++ js/layout.js
@@ -0,0 +1,222 @@
+﻿$(function(){
+	
+	 $(".pen").click(function () {
+        $(".popupbook").fadeIn(300);
+    });
+
+    $(".closebook").click(function () {
+        $(".popupbook").fadeOut(300);
+    });
+	
+	
+	//首页
+	$('.main ul li').each(function(){
+		var n = $(this).index()+1;
+		$(this).addClass('list'+n);
+	})
+
+	$('.qrcode').hover(function(){
+		$(this).siblings('.qrcodeimg').show();
+	},function(){
+		$(this).siblings('.qrcodeimg').hide();
+	})
+
+	$('.wx').hover(function(){
+		$(this).siblings('.qrcodeimg').show();
+	},function(){
+		$(this).siblings('.qrcodeimg').hide();
+	})
+
+	$('.nav ul h4').eq(0).css('overflow','hidden');
+
+	//产品详细页面
+	$('.colors dd div').click(function(){
+		$(this).addClass('cur').siblings().removeClass('cur')
+	})
+	$('.size a').click(function(){
+		$(this).addClass('act').siblings().removeClass('act')
+	})
+	var num = 1;
+	$('.number .add').click(function(){
+		num++;
+		$('.number em').html(num);
+	})
+
+	$('.number .cut').click(function(){
+		if ( num == 1) {
+			$('.number em').html(1);
+		}else{
+			num--;
+			$('.number em').html(num);
+		}
+	})
+	// 形象大片
+	var wLi1 = $('.menulist ul li').outerWidth(true),
+		len1 = $('.menulist ul li').length,
+		num1 = 0;
+	$('.menulist ul').css('width',wLi1*len1);
+
+	if ( len1 > 8 ) {
+		$('.video-list .prev').click(function(){
+			if (num1 == 0) {
+				$('.menulist ul').stop().animate({left:-num1*wLi1},600)
+			}else{
+				num1--
+				$('.menulist ul').stop().animate({left:-num1*wLi1},600)
+			}
+		})
+
+		$('.video-list .next').click(function(){
+			if (num1 == len1-8) {
+				$('.menulist ul').stop().animate({left:-num1*wLi1},600)
+			}else{
+				num1++
+				$('.menulist ul').stop().animate({left:-num1*wLi1},600)
+			}
+		})
+	}
+
+	// 杂志
+	var wLi2 = $('.magazinewrap ul li').outerWidth(true),
+		len2 = $('.magazinewrap ul li').length,
+		num2 = 0;
+	$('.magazinewrap ul').css('width',wLi2*len2);
+
+	$('.magazine-list .prev').click(function(){
+		if (num2 == 0) {
+			$('.magazinewrap ul').stop().animate({left:-num2*wLi2*4},600)
+		}else{
+			num2--
+			$('.magazinewrap ul').stop().animate({left:-num2*wLi2*4},600)
+		}
+	})
+
+	$('.magazine-list .next').click(function(){
+		if (num2 == Math.ceil(len2/4)-1) {
+			$('.magazinewrap ul').stop().animate({left:-num2*wLi2*4},600)
+		}else{
+			num2++
+			$('.magazinewrap ul').stop().animate({left:-num2*wLi2*4},600)
+		}
+	})
+
+	//产品详细
+
+	var len3 = $('.prodetails-left ul li').length,
+		num3 = 0;
+		str = '';
+	for ( var i=0; i<len3; i++){
+		str += "<a href='javascript:;'></a>"
+	}
+
+	$('.prodetails-left .dot').html(str);
+
+	$('.prodetails-left ul li').eq(0).show();
+	$('.prodetails-left .dot a').eq(0).addClass('cur');
+
+	$('.prodetails-left .dot a').click(function(){
+		num3 = $(this).index();
+		$(this).addClass('cur').siblings().removeClass('cur')
+		$('.prodetails-left ul li').eq(num3).show().siblings().hide();
+	});
+
+	$('.prodetails-left .next').click(function(){
+		if ( num3 == len3-1 ) {
+			num3 = 0
+		}else{
+			num3++;
+		}
+		$('.dot a').eq(num3).addClass('cur').siblings().removeClass('cur');
+		$('.prodetails-left ul li').eq(num3).stop().fadeIn().siblings().stop().fadeOut();
+	})
+
+	$('.prodetails-left .prev').click(function(){
+		if ( num3 == 0 ) {
+			num3 = len3-1;
+		}else{
+			num3--;
+		}
+		$('.dot a').eq(num3).addClass('cur').siblings().removeClass('cur');
+		$('.prodetails-left ul li').eq(num3).stop().fadeIn().siblings().stop().fadeOut();
+	}) 
+
+	$('.prodetails-left ul li img').mouseover(function(){
+		var src = $(this)[0].src;
+		$(this).parents('li').append("<div class=\"bigImg\">"+
+			"<img src=\""+ src +"\""+"/>"
+			+"</div>")
+	})
+
+	$('.prodetails-left ul li').mousemove(function(e){
+		var x = e.pageX - $(this).offset().left,
+			y = e.pageY - $(this).offset().top;
+			imgw = $('.bigImg img').outerWidth(),
+			imgh = $('.bigImg img').outerHeight();
+
+		var getX = x / $(this).outerWidth();
+		var getY = y / $(this).outerHeight();
+
+		$('.bigImg').css({
+			'left': -getX * (imgw - $(this).outerWidth()),
+			'top': -getY * (imgh - $(this).outerHeight())
+		})
+	})
+
+	$('.prodetails-left ul li').on('mouseout',(function(){
+		$(this).find('.bigImg').mouseout(function(){
+			$(this).stop().fadeOut('slow').remove();
+		})
+	}));
+
+	//杂志
+
+	
+
+    //下拉菜单
+	$('.subnav-con').eq(0).show()
+	$('.subnav-tit a').hover(function () {
+	    if ($(this).is('.tit1') != true) {
+	        var n = $(this).index();
+	        $('.subnav-con').eq(n - 1).show().siblings().hide();
+	    }
+	})
+
+	var hiddenThisId = $(".hiddenThisId").val();
+	var hiddenThisParentId = $(".hiddenThisParentId").val();
+	$(".productLeftNav li[rel='"+hiddenThisId+"']").find(".prolist").show();
+	$(".productLeftNav li[rel='"+hiddenThisParentId+"']").find(".prolist").show();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
